3dc4cb8ec478aeed4679769d65f5d380c02d33e6
Ethan Counasse <ehcounasse@usf.edu>
2025-09-24 20:21:34 -0400

    Return Decimal Rate Function

--- ic_5.js
+++ ic_5.js
@@ -17,3 +17,28 @@ const cart = [
 
 // Try: "regular", "student", "member", or "vip"
 const customerType = "member";
+
+
+function getCategoryDiscount(category) {
+     switch (category) {
+        case 'gear':
+            return .1
+            break
+            case "apparel" :
+            return .15
+            break
+            case "snacks":
+            return .08
+            break
+            case "access":
+            return .05
+            break 
+            default: 
+            return 0
+            break
+    }
+
+    }
+
+    
+     
